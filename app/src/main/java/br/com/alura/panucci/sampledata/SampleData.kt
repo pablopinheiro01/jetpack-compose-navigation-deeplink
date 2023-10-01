372b08b0dfad249a5a47ee98cdc9bafb71ff2e98
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2023-10-01 17:29:32 -0300

    add random value and prepare to receive the not equal value about the item special for promotion

--- app/src/main/java/br/com/alura/panucci/sampledata/SampleData.kt
+++ app/src/main/java/br/com/alura/panucci/sampledata/SampleData.kt
@@ -3,12 +3,14 @@ package br.com.alura.panucci.sampledata
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import br.com.alura.panucci.model.Product
 import java.math.BigDecimal
+import java.util.UUID
 import kotlin.random.Random
 
 private val loremName = LoremIpsum(Random.nextInt(10)).values.first()
 private val loremDesc = LoremIpsum(Random.nextInt(30)).values.first()
 
 val sampleProductWithImage = Product(
+    id = UUID.randomUUID().toString(),
     name = LoremIpsum(10).values.first(),
     price = BigDecimal("9.99"),
     description = LoremIpsum(30).values.first(),
@@ -16,15 +18,23 @@ val sampleProductWithImage = Product(
 )
 
 val sampleProductWithoutImage = Product(
+    id = UUID.randomUUID().toString(),
     name = LoremIpsum(10).values.first(),
     price = BigDecimal("9.99"),
     description = LoremIpsum(30).values.first(),
 )
 
 val sampleProducts = List(10) { index ->
+    val id=if(index ==0){
+        "9adccd9a-3918-4996-8c96-2f5b9143cef2"
+    }else{
+        UUID.randomUUID().toString()
+    }
+    val price = if(index == 0) BigDecimal("299.90") else BigDecimal("10.00")
     Product(
+        id = id,
         name = loremName,
-        price = BigDecimal("9.99"),
+        price = price,
         description = loremDesc,
         image = if (index % 2 == 0) "https://picsum.photos/1920/1080" else null
     )
